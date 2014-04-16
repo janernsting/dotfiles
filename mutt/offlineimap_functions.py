796fb849b8f5a908572bab8439b714e2f15ee738
Jan Ernsting <jan.ernsting@gmail.com>
2014-04-16 11:57:12 +0200

    Add conversion functions

--- mutt/offlineimap_functions.py
+++ mutt/offlineimap_functions.py
@@ -1,8 +1,33 @@
 #!/usr/bin/env python
 import gnomekeyring
+import re
 
 def get_secret(display_name, keyring_name = "login"):
   for id in gnomekeyring.list_item_ids_sync(keyring_name):
     item = gnomekeyring.item_get_info_sync(keyring_name, id)
     if display_name == item.get_display_name():
       return item.get_secret()
+
+top_level_folders = ['Drafts', 'Sent', 'Trash']
+
+def convert_to_local(folder):
+  folder = re.sub('INBOX\/', '', folder)
+  folder = re.sub(' ', '_', folder)
+  folder = first_to_lower(folder) if folder in top_level_folders else folder
+  return folder
+
+def first_to_lower(s):
+  return s[0].lower() + s[1:] if s else None
+
+def convert_to_remote(folder):
+  folder = first_to_upper(folder) if first_to_upper(folder) in top_level_folders else folder
+  folder = re.sub('_', ' ', folder)
+  folder = "INBOX/" + folder if folder not in top_level_folders else folder
+  return folder
+
+def first_to_upper(s):
+  return s[0].upper() + s[1:] if s else None
+
+def filter_folder(folder):
+  return not folder.startswith('Synchronisierungsprobleme') \
+      and folder not in ['Junk-E-Mail', 'Postausgang']
---
+++ mutt/test_offlineimap_functions.py
@@ -0,0 +1,63 @@
+from hamcrest import *
+import unittest
+import offlineimap_functions as funcs
+
+
+class RemoteToLocalTest(unittest.TestCase):
+  def convert(self, folder):
+    return funcs.convert_to_local(folder)
+
+  def assert_converts_to(self, actual_folder, expected_folder):
+    assert_that(self.convert(actual_folder), equal_to(expected_folder))
+
+  def testInboxRemainsUnchanged(self):
+    inbox = 'INBOX'
+    self.assert_converts_to(inbox, inbox)
+
+  def testSubfolderBecomesFolder(self):
+    subfolder = 'INBOX/Archiv'
+    folder = 'Archiv'
+    self.assert_converts_to(subfolder, folder)
+
+  def testChangeCaseForTopLevelFolders(self):
+    self.assert_converts_to('Drafts', 'drafts')
+    self.assert_converts_to('Sent', 'sent')
+    self.assert_converts_to('Trash', 'trash')
+
+  def testConvertSpacesToUnderscores(self):
+    self.assert_converts_to('INBOX/EAI 2013', 'EAI_2013')
+
+
+class LocalToRemoteTest(unittest.TestCase):
+  def convert(self, folder):
+    return funcs.convert_to_remote(folder)
+
+  def assert_converts_to(self, actual_folder, expected_folder):
+    assert_that(self.convert(actual_folder), equal_to(expected_folder))
+
+  def testChangeCaseForTopLevelFolders(self):
+    self.assert_converts_to('drafts', 'Drafts')
+    self.assert_converts_to('sent', 'Sent')
+    self.assert_converts_to('trash', 'Trash')
+
+  def testConvertOtherFolderToSubfolder(self):
+    self.assert_converts_to('archive', 'INBOX/archive')
+
+  def testConvertUnderscoresToSpaces(self):
+    self.assert_converts_to('EAI_2013', 'INBOX/EAI 2013')
+
+
+class FolderfilterTest(unittest.TestCase):
+  def testExcludeSyncProblems(self):
+    assert_that(not funcs.filter_folder('Synchronisierungsprobleme'))
+    assert_that(not funcs.filter_folder('Synchronisierungsprobleme.Konflikte'))
+
+  def testExcludeJunkEMail(self):
+    assert_that(not funcs.filter_folder('Junk-E-Mail'))
+
+  def testExcludePostausgang(self):
+    assert_that(not funcs.filter_folder('Postausgang'))
+
+  def testIncludeOthers(self):
+    assert_that(funcs.filter_folder('INBOX'))
+    assert_that(funcs.filter_folder('Sent'))
