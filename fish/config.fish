c07f38e44ad8812b002406caf3f25a17e27c56bd
Jan Ernsting <jan.ernsting@gmail.com>
2015-07-19 17:46:52 +0200

    Default to git short status with no commands

--- fish/config.fish
+++ fish/config.fish
@@ -9,3 +9,12 @@ end
 function mutt
   bash --login -c '/usr/bin/mutt' $argv;
 end
+
+function git
+  # inspired by thoughtbot/dotfiles
+  if count $argv > /dev/null # alternative: set -q argv
+    command git $argv
+  else
+    command git status -sb
+  end
+end
