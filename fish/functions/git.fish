cde387d095978ec46abc9d003e7978ae0485a184
Jan Ernsting <jan.ernsting@gmail.com>
2015-07-31 20:45:07 +0200

    Add abbreviations for git

--- fish/functions/git.fish
+++ fish/functions/git.fish
@@ -1,4 +1,11 @@
 abbr -a g git
+abbr -a gc git commit -v
+abbr -a ga git add
+abbr -a gco git checkout
+abbr -a gr git remote
+abbr -a grh git reset HEAD
+abbr -a grhh git reset HEAD --hard
+abbr -a lg git lg
 
 function git
   # inspired by thoughtbot/dotfiles
