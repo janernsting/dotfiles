27ff64086e0fa7592e05cb1b80d1ce4c866a0309
Jan Ernsting <jan.ernsting@gmail.com>
2017-09-22 15:30:16 +0200

    Export byobu prefix

--- fish/config.fish
+++ fish/config.fish
@@ -12,3 +12,4 @@ set fish_key_bindings fish_vi_key_bindings
 eval (perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/python/libexec/bin:$PATH"
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+export BYOBU_PREFIX=/usr/local
