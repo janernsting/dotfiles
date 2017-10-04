31591ac8adbd8c436e4333d207aad27ddf2595bb
Jan Ernsting <jan.ernsting@gmail.com>
2017-10-04 19:09:10 +0200

    Remove duplicate key bindings
    These are also defined in the shared keybindings of fish.

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,14 +1,7 @@
 function fish_user_key_bindings
   bind --mode insert --sets-mode default kj backward-char force-repaint
 
-  bind -M visual -m insert c kill-selection end-selection force-repaint
-
-  bind \cl 'clear; commandline -f repaint'
-  bind -M insert \cl 'clear; commandline -f repaint'
-
   bind -M insert \cf forward-word
-
-  bind \cu backward-kill-line
 
   bind -M default V edit_command
   bind -M default K commandline_man
