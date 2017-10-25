8a3b2c25ff5d17ce4120c254720307c3851fbddf
Jan Ernsting <jan.ernsting@gmail.com>
2017-10-25 16:15:13 +0200

    Move abbreviations into a dedicated file
    Abbreviations have to be initialised just once. They are stored in a
    universal variable.
    
    Quote from the documentation:
    > If you keep the variable as universal, abbr --add statements in
    > config.fish will do nothing but slow down startup slightly.
    
    https://fishshell.com/docs/current/commands.html#abbr

--- fish/functions/git.fish
+++ fish/functions/git.fish
@@ -1,12 +1,3 @@
-abbr -a g git
-abbr -a gc git commit -v
-abbr -a ga git add
-abbr -a gco git checkout
-abbr -a gr git remote
-abbr -a grh git reset HEAD
-abbr -a grhh git reset HEAD --hard
-abbr -a lg git lg
-
 function git
   # inspired by thoughtbot/dotfiles
   if count $argv > /dev/null # alternative: set -q argv
---
+++ fish/functions/initialise_abbreviations.fish
@@ -0,0 +1,9 @@
+# git abbreviations
+abbr -a g git
+abbr -a gc git commit -v
+abbr -a ga git add
+abbr -a gco git checkout
+abbr -a gr git remote
+abbr -a grh git reset HEAD
+abbr -a grhh git reset HEAD --hard
+abbr -a lg git lg
