5997bde55506e9bd75a8677eb8206e163b7d1c1b
Jan Ernsting <jan.ernsting@gmail.com>
2018-03-06 11:15:49 +0100

    Adjust to python3 conventions

--- mutt/getsecret.py
+++ mutt/getsecret.py
@@ -4,8 +4,8 @@ import keyring
 import sys
  
 def display_usage():
-  print "Usage:"
-  print "%s display_name" % sys.argv[0]
+  print("Usage:")
+  print("%s display_name" % sys.argv[0])
 
 if __name__ == '__main__':
   if len(sys.argv) == 1:
@@ -13,4 +13,4 @@ if __name__ == '__main__':
     sys.exit(1)
 
   display_name = sys.argv[1]
-  print keyring.get_password(display_name, display_name)
+  print(keyring.get_password(display_name, display_name))
