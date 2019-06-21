9a8eb9a29ce48cd62b0456607d8ef602afc5cddc
Jan Ernsting <jan.ernsting@gmail.com>
2019-06-21 14:25:19 +0200

    Add gnu-sed to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -10,7 +10,7 @@ set -gx EDITOR vim
 
 set fish_key_bindings fish_vi_key_bindings
 eval (perl -I$HOME/perl5/lib/perl5 -Mlocal::lib=$HOME/perl5)
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/python/libexec/bin:$PATH"
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/gnu-sed/libexec/gnubin:/usr/local/opt/python/libexec/bin:$PATH"
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 export BYOBU_PREFIX=/usr/local
 export GPG_TTY=(tty)
